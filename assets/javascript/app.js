6ed46badca1951193253e5bedeab6e1446cc19fd
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2017-12-14 13:36:21 -0500

    push

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//create a questions and answers array
+//use cartoon/nick characters for trivia
+//get gifs to make the game more engaging
+//**don't forget increment seconds */
+//use game reset as well
+alert("HEllo World")
+console.log("Test me!")
