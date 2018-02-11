52a6acc3f307151bfa184d36debb94c3b36b5fe6
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-02-10 20:32:20 -0500

    added stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,363 +1,191 @@
-//create a questions and answers array
-//use cartoon/nick characters for trivia
-//get gifs to make the game more engaging
-//**don't forget increment seconds */
-//use game reset as well
-
-var timer;
-
-var trivia = {
-	// Set the boundaries of the game
-	time: 15,	
-	correct: 0,
-	incorrect: 0,
-	unanswered: 0,
-	//Four answers nested within Question strings
-	question: {
-		one: {
-			string: "What is the name of Dexter's sister?",
-			answer: {
-				one: {
-					string: "Dee Dee",
-					value: true
-				},
-				two: {
-					string: "Mee Mee",
-					value: false
-				},
-				three: {
-					string: "Lee Lee",
-					value: false
-				},
-				four: {
-					string: "Olga",
-					value: false
-				}
-			},
-			gif: "assets/images/deedee.webp"
-		},
-		two: {
-			string: "Who is the main trainer in the Pokemon series?",
-			answer: {
-				one: {
-					string: "Red",
-					value: false
-				},
-				two: {
-					string: "Pikachu",
-					value: false
-				},
-				three: {
-					string: "Ash",
-					value: true
-				},
-				four: {
-					string: "Gory",
-					value: false
-				}
-			},
-			gif: "assets/images/ash.gif"
-		},
-		three: {
-			string: "Where does Spongebob Squarepants work?",
-			answer: {
-				one: {
-					string: "Central Perk",
-					value: false
-				},
-				two: {
-					string: "The Krusty Krab",
-					value: true
-				},
-				three: {
-					string: "The Greasy Spoon",
-					value: false
-				},
-				four: {
-					string: "Chum Bucket",
-					value: false
-				}
-			},
-			gif: "assets/images/kkrab.gif"
-		},
-		four: {
-			string: "What is the name of the dog belonging to the Simpsons?",
-			answer: {
-				one: {
-					string: "Santa's Little Helper",
-					value: true
-				},
-				two: {
-					string: "Jake the dog",
-					value: false
-				},
-				three: {
-					string: "Ruff",
-					value: false
-				},
-				four: {
-					string: "Boy",
-					value: false
-				}
-			},
-			gif: "assets/images/santa.gif"
-		},
-		five: {
-			string: "Who lives in the giant head between Spongebob & Patrick?",
-			answer: {
-				one: {
-					string: "Mr. Krabs",
-					value: false
-				},
-				two: {
-					string: "Plankton",
-					value: false
-				},
-				three: {
-					string: "Sandy",
-					value: false
-				},
-				four: {
-					string: "Squidward",
-					value: true
-				}
-			},
-			gif: "assets/images/squid.gif"
-		},
-		six: {
-			string: "Who are the two main characters of Adventure Time?",
-			answer: {
-				one: {
-					string: "Timmy & Vicky",
-					value: false
-				},
-				two: {
-					string: "Spongebob & Patrick",
-					value: false
-				},
-				three: {
-					string: "Finn & Jake",
-					value: true
-				},
-				four: {
-					string: "Kenon & Kel",
-					value: false
-				}
-			},
-			gif: "assets/images/finn.gif"
-		},
-		seven: {
-			string: "Which character was often referred to as 'football head'?",
-			answer: {
-				one: {
-					string: "Arnold",
-					value: true
-				},
-				two: {
-					string: "Dexter",
-					value: false
-				},
-				three: {
-					string: "Spongebob",
-					value: false
-				},
-				four: {
-					string: "Pink Panther",
-					value: false
-				}
-			},
-			gif: "assets/images/arnold.gif"
-		},
-		eight: {
-			string: "Which sailor charactor would be lost without his can of spinach?",
-			answer: {
-				one: {
-					string: "Mickey Mouse",
-					value: false
-				},
-				two: {
-					string: "Popeye",
-					value: true
-				},
-				three: {
-					string: "Juniper Lee",
-					value: false
-				},
-				four: {
-					string: "Betty Bop",
-					value: false
-				}
-			},
-			gif: "assets/images/popeye.gif"
-		},
-		nine: {
-			string: "Who is Batman's side-kick?",
-			answer: {
-				one: {
-					string: "Ice Man",
-					value: false
-				},
-				two: {
-					string: "Tarzan",
-					value: false
-				},
-				three: {
-					string: "Robin",
-					value: true
-				},
-				four: {
-					string: "Aqua Man",
-					value: false
-				}
-			},
-			gif: "assets/images/robin.gif"
-		},
-		ten: {
-			string: "Where do the Simpsons live?",
-			answer: {
-				one: {
-					string: "Bikini Bottom",
-					value: false
-				},
-				two: {
-					string: "Springfield",
-					value: true
-				},
-				three: {
-					string: "Spooner St.",
-					value: false
-				},
-				four: {
-					string: "City of Townsville",
-					value: false
-				}
-			},
-			gif: "assets/images/spring.gif"
-		}
-	},
-
-	//Method that starts the timer
-	startTimer: function() {
-		trivia.time = 15;
-		$("#timer").text("Time Remaining: " + trivia.time + " seconds");
-		timer = setInterval(trivia.countDown, 1000)
-	},
-
-	//Method that stops the timer
-	stopTimer: function() {
-		clearInterval(timer);
-	},
-	// When time expires this method will activate
-	timeOver: function() {
-		this.unanswered++;		
-		$(".answer").hide();
-		$("#question").html("Whoops, every second counts! The correct answer is:<br>");
-		$("[value='true']").contents().clone().appendTo($("#question"));
-		$("#question").append("<br><img src='" + questions[i].gif + "'>");
-		this.nextQuestion();
-	},
-	// Counts down the game timer a second at a time
-	countDown: function() {
-		trivia.time--;
-		$("#timer").text("Time Remaining: " + trivia.time + " seconds");
-
-		if (trivia.time === 0) {
-			trivia.stopTimer();
-			trivia.timeOver();
-		}
-	},
-
-	startQuiz: function() {
-		i = 0;
-		this.correct;
-		this.incorrect;
-		this.unanswered;
-		trivia.questionStart(questions[i]);
-		$("#start").hide();
-	},
-
-	//Calls the previously assigned Qs and As to appear in "index.html"
-	questionStart: function(q) {
-		$(".answer").show();
-		$("#question").text(q.string);
-		$("#optionA").text(q.answer.one.string);
-		$("#optionB").text(q.answer.two.string);
-		$("#optionC").text(q.answer.three.string);
-		$("#optionD").text(q.answer.four.string);
-
-		$("#optionA").attr("value", q.answer.one.value);
-		$("#optionB").attr("value", q.answer.two.value);
-		$("#optionC").attr("value", q.answer.three.value);
-		$("#optionD").attr("value", q.answer.four.value);
-
-		this.startTimer();
-	},
-
-	rightAnswer: function() {
-		this.correct++;
-		$(".answer").hide();
-		$("#question").html("Yay!");
-		$("#question").append("<br><img src='" + questions[i].gif + "'>");
-		this.nextQuestion();
-		
-	},
-
-	wrongAnswer: function() {
-		this.incorrect++;
-		$(".answer").hide();
-		$("#question").html("You missed it! Correct Answer:<br>");
-		$("[value='true']").contents().clone().appendTo($("#question"));
-		$("#question").append("<br><img src='" + questions[i].gif + "'>");
-		this.nextQuestion();
-	},
-
-	nextQuestion: function() {
-		i++
-		this.stopTimer();
-		if (i < questions.length){
-			setTimeout(function(){trivia.questionStart(questions[i])}, 1000*9);
-		}
-		else {
-			setTimeout(function(){trivia.endQuiz()}, 1000*10);
-		}
-	},
-
-	endQuiz: function() {
-		$("#start").show()
-		$("#question").text("Game Over! Try to beat your scores below:")
-		$("#question").append("<p>Correct: " + this.correct + "</p>");
-		$("#question").append("<p>Incorrect: " + this.incorrect + "</p>");
-		$("#question").append("<p>Time-outs " + this.unanswered + "</p>");
-	}
-};
-// Loop that cycles through the ten questions to dynamically apply content to the html
-for (var j = 0; j < 10; j++) {
-	var number = Math.floor(Math.random() * 4) + 1;
-	console.log(number);
+var triviaQuestions = [{
+	question: "What is Sulley's full name?",
+	answerList: ["Michael J. Sullivan", "James P. Sullivan", "Sully Sullivan", "Jeff"],
+	answer: 1
+},{
+	question: "What kind of bug is Frances from 'A Bug's Life'?",
+	answerList: ["Ladybug", "Ant", "Grasshopper", "Caterpillar"],
+	answer: 0
+},{
+	question: "What's the name of Woody's boy?",
+	answerList: ["Andy", "Danny", "Max", "Sid"],
+	answer: 0
+},{
+	question: "Merida's mother changes into what after eating the enchanted cake?",
+	answerList: ["Dragon", "Witch", "Bear", "Will-o'-the-Wisp"],
+	answer: 2
+},{
+	question: "What precious cargo is Wall-E protecting?",
+	answerList: ["trash", "photos", "gold", "a plant"],
+	answer: 3
+},{
+	question: "Which of the following is not an emotion showed in 'Inside Out'?",
+	answerList: ["Confusion", "Sadness", "Fear", "Disgust"],
+	answer: 0
+},{
+	question: "What is the name of Mr. Incredible's nemesis?",
+	answerList: ["Frozone", "Syndrome", "Mirage", "Elastigirl"],
+	answer: 1
+},{
+	question: "What is Nemo's mom's name?",
+	answerList: ["Pearl", "Deb", "Coral", "Dory"],
+	answer: 2
+},{
+	question: "What is the big race called in 'Cars'?",
+	answerList: ["Daytona 500", "Piston Cup", "Radiator Springs", "Route 66"],
+	answer: 1
+},{
+	question: "In 'Up', what is Russell trying to earn a badge for?",
+	answerList: ["Wild Wilderness Scouts", "Explorers of America", "Wilderness Explorer", "Boy Scouts"],
+	answer: 2
+},{
+	question: "From 'The Good Dinosaur,' what does Arlo call his pet?",
+	answerList: ["Spunky", "Spot", "Sparky", "Fido"],
+	answer: 1
+},{
+	question: "What fraternity does Mike Wazowski end up joining in college?",
+	answerList: ["Zeta Hiss Alpha", "Python Nu Kappa", "Roar Omega Roar", "Oozma Kappa"],
+	answer: 3
+},{
+	question: "Which of the following ISN'T a phrase when you pull on Woody's drawstring?",
+	answerList: ["Reach for the sky!", "Ride like the wind, Bullseye!", "There's a snake in my boot!", "Somebody's poisoned the water hole!"],
+	answer: 1
+},{
+	question: "Who's your friend who like's to play...?",
+	answerList: ["Bong Bong, Bong Bong", "Ding Dong, Ding Dong", "Bing Bong, Bing Bong", "Sing Song, Sing Song"],
+	answer: 2
+},{
+	question: "Do you think it's odd that 'Ratatouille' takes place is Paris, but the two main characters don't have accents?",
+	answerList: ["Yes! So weird!", "They don't have accents?", "Nope!", "What's a Ratatouille?"],
+	answer: 0
+}];
+var search = ['sully+monsters+inc', 'bugs+life', 'toy+story+andy', 'brave+bear', 'wall-e+plant', 'inside+out+emotions', 'the+incredibles+nemesis', 'finding+nemo', 'lightning+mcqueen', 'up+russell', 'the+good+dinosaur', 'monsters+university', 'toy+story+woody','bing+bong','ratatouille'];
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "Yes, that's right!",
+	incorrect: "No, that's not it.",
+	endTime: "Out of time!",
+	finished: "Alright! Let's see how well you did."
 }
 
-var tQ = trivia.question;
-var questions = [tQ.one, tQ.two, tQ.three, tQ.four, tQ.five, tQ.six, tQ.seven, tQ.eight, tQ.nine, tQ.ten]
-
-// Hides the answer until we need it revealed
-$(".answer").hide();
-
-//Hitting the start button will begin the quiz. The 15 second timer will kickoff for each individual question.
-$("#start").click(function() {
-	trivia.startQuiz();
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
 });
 
-//Choosing the correct answer will stop the timer, and replace the question and answers with a correct answer message, and an animated gif
-$(".answer").click(function() {
-	console.log(this);
-	console.log($(this).attr("value"));
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
 
-	var check = $(this).attr("value");
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
+
+function newQuestion(){
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+	answered = true;
 	
-	if (check === "true") {
-		trivia.rightAnswer();
+	//sets up new questions & answerList
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
 	}
-	if (check === "false") {
-		trivia.wrongAnswer();
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+
+function countdown(){
+	seconds = 15;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
 	}
-});
+}
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	//giphy api
+	var giphyURL = "http://api.giphy.com/v1/gifs/search?q=pixar+" + search[currentQuestion] + "&limit=1&rating=g&api_key=dc6zaTOxFJmzC"
+	$.ajax({url: giphyURL, method: 'GET'}).done(function(giphy){
+		var currentGif = giphy.data;
+		$.each(currentGif, function(index,value){
+		var embedGif = value.images.original.url;
+		newGif = $('<img>');
+		newGif.attr('src', embedGif);
+		newGif.addClass('gifImg');
+		$('#gif').html(newGif);
+		});
+	});
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}	
+}
+
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
